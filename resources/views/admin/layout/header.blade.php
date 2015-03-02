7f24a7b8ff6f42e98eb47eb2227922af5a503c38
mihaibarbu <xwave21@gmail.com>
2015-03-02 13:09:20 +0200

    Add admin controller and view

---
+++ resources/views/admin/layout/header.blade.php
@@ -0,0 +1,25 @@
+<div class="container">
+            <!-- Static navbar -->
+<nav class="navbar navbar-inverse" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/admin/">Panou administrare</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class=""><a href="/admin/entry">Aproba inscrieri</a></li>
+                <li class=""><a href="/admin/entry/disapprove">Dezaproba inscrieri</a></li>
+                <li class=""><a href="/admin/user">Adauga Admin</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/admin/entry/logout">Logout</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div><!--/.container-fluid -->
+</nav>
---
+++ resources/views/admin/layout/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Admin Area</title>
+
+		<link href="/css/app.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			@include('admin.layout.header')
+			@yield('content')
+		</div>
+	</body>
+</html>
