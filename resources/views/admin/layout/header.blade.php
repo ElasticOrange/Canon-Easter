b6774fdabdf5e1d6bea8450ac36ecf4d31da5ec7
mihaibarbu <xwave21@gmail.com>
2015-03-19 11:06:09 +0200

    Add migrations create admins table

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -9,7 +9,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/admin/login">Panou administrare</a>
+            <a class="navbar-brand" href="/admin/entry">Panou administrare</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
